9e0e9369042ff2ff014f46697c3cca2d68834ac6
nanshenjiang <979114715@qq.com>
2023-03-26 18:10:11 +0800

    feat: perform of file encrypt

---
+++ test/perform_file_enc_test.c
@@ -0,0 +1,335 @@
+#include <wbcrypto/sm4.h>
+#include <wbcrypto/aes.h>
+#include <wbcrypto/wbaes.h>
+#include <wbcrypto/wbsm4.h>
+#include <wbcrypto/wbaes_wbgcm_ee.h>
+#include <wbcrypto/wbaes_wbgcm_mask.h>
+#include "test_local.h"
+
+static const unsigned char key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                                      0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+
+const char *testFilePath[] = {"../../testData/test_1KB.txt", "../../testData/test_10KB.txt",
+                              "../../testData/test_100KB.txt", "../../testData/test_1MB.txt",
+                              "../../testData/test_10MB.txt", "../../testData/test_100MB.txt"};
+
+int power(int base, int exponent) {
+    int result = 1;
+    int i;
+    for (i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+int test_aes_gcmfile() {
+    int i, j;
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
+    clock_t program_start, program_end;
+    double ts;
+
+    WBCRYPTO_aes_context *aes_ctx = WBCRYPTO_aes_context_init();
+    WBCRYPTO_aes_init_key(aes_ctx, key, sizeof(key));
+
+    WBCRYPTO_gcmfile_context *enc_gcmfile, *dec_gcmfile;
+    enc_gcmfile = WBCRYPTO_aes_gcmfile_init(aes_ctx);
+    WBCRYPTO_gcmfile_setiv(enc_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(enc_gcmfile, aad, sizeof(aad));
+    dec_gcmfile = WBCRYPTO_aes_gcmfile_init(aes_ctx);
+    WBCRYPTO_gcmfile_setiv(dec_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(dec_gcmfile, aad, sizeof(aad));
+
+    for (i = 0; i < sizeof(testFilePath) / 8; i++) {
+        char *tmp_enc = "../../testData/test.enc.txt";
+        char *tmp_dec = "../../testData/test.dec.txt";
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_encrypt(enc_gcmfile, testFilePath[i], tmp_enc);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("AES encrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_decrypt(dec_gcmfile, tmp_enc, tmp_dec);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("AES decrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+        remove(tmp_enc);
+        remove(tmp_dec);
+    }
+
+    WBCRYPTO_aes_context_free(aes_ctx);
+    WBCRYPTO_gcmfile_free(enc_gcmfile);
+    WBCRYPTO_gcmfile_free(dec_gcmfile);
+    return 1;
+}
+
+int test_sm4_gcmfile() {
+    int i,j;
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
+    clock_t program_start, program_end;
+    double ts;
+
+    WBCRYPTO_sm4_context *sm4_ctx = WBCRYPTO_sm4_context_init();
+    WBCRYPTO_sm4_init_key(sm4_ctx, key, sizeof(key));
+
+    WBCRYPTO_gcmfile_context *enc_gcmfile, *dec_gcmfile;
+    enc_gcmfile = WBCRYPTO_sm4_gcmfile_init(sm4_ctx);
+    WBCRYPTO_gcmfile_setiv(enc_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(enc_gcmfile, aad, sizeof(aad));
+    dec_gcmfile = WBCRYPTO_sm4_gcmfile_init(sm4_ctx);
+    WBCRYPTO_gcmfile_setiv(dec_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(dec_gcmfile, aad, sizeof(aad));
+
+    for (i = 0; i < sizeof(testFilePath) / 8; i++) {
+        char *tmp_enc = "../../testData/test.enc.txt";
+        char *tmp_dec = "../../testData/test.dec.txt";
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_encrypt(enc_gcmfile, testFilePath[i], tmp_enc);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("SM4 encrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_decrypt(dec_gcmfile, tmp_enc, tmp_dec);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("SM4 decrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+        remove(tmp_enc);
+        remove(tmp_dec);
+    }
+
+    WBCRYPTO_sm4_context_free(sm4_ctx);
+    WBCRYPTO_gcmfile_free(enc_gcmfile);
+    WBCRYPTO_gcmfile_free(dec_gcmfile);
+    return 1;
+}
+
+int test_wbaes_gcmfile() {
+    int i,j;
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
+    clock_t program_start, program_end;
+    double ts;
+
+    WBCRYPTO_wbaes_context *wbaes_ctx;
+    wbaes_ctx = WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
+    WBCRYPTO_wbaes_gen_table(wbaes_ctx, key, sizeof(key));
+
+    WBCRYPTO_gcmfile_context *enc_gcmfile, *dec_gcmfile;
+    enc_gcmfile = WBCRYPTO_wbaes_gcmfile_init(wbaes_ctx);
+    WBCRYPTO_gcmfile_setiv(enc_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(enc_gcmfile, aad, sizeof(aad));
+    dec_gcmfile = WBCRYPTO_wbaes_gcmfile_init(wbaes_ctx);
+    WBCRYPTO_gcmfile_setiv(dec_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(dec_gcmfile, aad, sizeof(aad));
+
+    for (i = 0; i < sizeof(testFilePath) / 8; i++) {
+        char *tmp_enc = "../../testData/test.enc.txt";
+        char *tmp_dec = "../../testData/test.dec.txt";
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_encrypt(enc_gcmfile, testFilePath[i], tmp_enc);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBAES encrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_decrypt(dec_gcmfile, tmp_enc, tmp_dec);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBAES decrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+        remove(tmp_enc);
+        remove(tmp_dec);
+    }
+
+    WBCRYPTO_wbaes_context_free(wbaes_ctx);
+    WBCRYPTO_gcmfile_free(enc_gcmfile);
+    WBCRYPTO_gcmfile_free(dec_gcmfile);
+    return 1;
+}
+
+int test_wbsm4_gcmfile() {
+    int i,j;
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
+    clock_t program_start, program_end;
+    double ts;
+
+    WBCRYPTO_wbsm4_context *wbsm4_ctx;
+    wbsm4_ctx = WBCRYPTO_wbsm4_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
+    WBCRYPTO_wbsm4_gen_table(wbsm4_ctx, key, sizeof(key));
+
+    WBCRYPTO_gcmfile_context *enc_gcmfile, *dec_gcmfile;
+    enc_gcmfile = WBCRYPTO_wbsm4_gcmfile_init(wbsm4_ctx);
+    WBCRYPTO_gcmfile_setiv(enc_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(enc_gcmfile, aad, sizeof(aad));
+    dec_gcmfile = WBCRYPTO_wbsm4_gcmfile_init(wbsm4_ctx);
+    WBCRYPTO_gcmfile_setiv(dec_gcmfile, iv, sizeof(iv));
+    WBCRYPTO_gcmfile_aad(dec_gcmfile, aad, sizeof(aad));
+
+    for (i = 0; i < sizeof(testFilePath) / 8; i++) {
+        char *tmp_enc = "../../testData/test.enc.txt";
+        char *tmp_dec = "../../testData/test.dec.txt";
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_encrypt(enc_gcmfile, testFilePath[i], tmp_enc);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBSM4 encrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_gcmfile_decrypt(dec_gcmfile, tmp_enc, tmp_dec);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBSM4 decrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+        remove(tmp_enc);
+        remove(tmp_dec);
+    }
+
+    WBCRYPTO_wbsm4_context_free(wbsm4_ctx);
+    WBCRYPTO_gcmfile_free(enc_gcmfile);
+    WBCRYPTO_gcmfile_free(dec_gcmfile);
+    return 1;
+}
+
+int test_wbaes_wbgcmfile_ee() {
+    int i,j;
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
+    clock_t program_start, program_end;
+    double ts;
+
+    WBCRYPTO_wbaes_wbgcm_ee_context wbaes_ctx;
+    WBCRYPTO_wbaes_wbgcm_ee_gen_table(&wbaes_ctx, key, sizeof(key));
+
+    WBCRYPTO_wbgcm_ee_context *wbgcm_enc = WBCRYPTO_wbaes_wbgcm_ee_init(&wbaes_ctx);
+    WBCRYPTO_wbaes_wbgcm_ee_setiv(wbgcm_enc, iv, sizeof(iv));
+    WBCRYPTO_wbaes_wbgcm_ee_aad(wbgcm_enc, aad, sizeof(aad));
+    WBCRYPTO_wbgcm_ee_context *wbgcm_dec = WBCRYPTO_wbaes_wbgcm_ee_init(&wbaes_ctx);
+    WBCRYPTO_wbaes_wbgcm_ee_setiv(wbgcm_dec, iv, sizeof(iv));
+    WBCRYPTO_wbaes_wbgcm_ee_aad(wbgcm_dec, aad, sizeof(aad));
+
+    for (i = 0; i < sizeof(testFilePath) / 8; i++) {
+        char *tmp_enc = "../../testData/test.enc.txt";
+        char *tmp_dec = "../../testData/test.dec.txt";
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_wbaes_wbgcmfile_ee_encrypt(wbgcm_enc, WBCRYPTO_TEST_FIN_PATH, WBCRYPTO_TEST_FENC_PATH);
+
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBSM4-EE encrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_wbaes_wbgcmfile_ee_decrypt(wbgcm_dec, WBCRYPTO_TEST_FENC_PATH, WBCRYPTO_TEST_FDEC_PATH);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBSM4-EE decrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+        remove(tmp_enc);
+        remove(tmp_dec);
+    }
+
+    WBCRYPTO_wbaes_wbgcm_ee_free(wbgcm_enc);
+    WBCRYPTO_wbaes_wbgcm_ee_free(wbgcm_dec);
+    return 1;
+}
+
+int test_wbaes_wbgcmfile_mask() {
+    int i,j;
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
+    clock_t program_start, program_end;
+    double ts;
+
+    WBCRYPTO_wbaes_wbgcm_mask_context *wbaes_enc_ctx = WBCRYPTO_wbaes_wbgcm_mask_init_table();
+    WBCRYPTO_wbaes_wbgcm_mask_context *wbaes_dec_ctx = WBCRYPTO_wbaes_wbgcm_mask_init_table();
+    WBCRYPTO_wbaes_wbgcm_mask_gen_table(wbaes_enc_ctx, wbaes_dec_ctx, key, sizeof(key));
+
+    WBCRYPTO_wbgcm_mask_context *wbgcm_enc;
+    wbgcm_enc = WBCRYPTO_wbaes_wbgcm_mask_init(wbaes_enc_ctx);
+    WBCRYPTO_wbaes_wbgcm_mask_setiv(wbgcm_enc, iv, sizeof(iv));
+    WBCRYPTO_wbaes_wbgcm_mask_aad(wbgcm_enc, aad, sizeof(aad));
+    WBCRYPTO_wbgcm_mask_context *wbgcm_dec;
+    wbgcm_dec = WBCRYPTO_wbaes_wbgcm_mask_init(wbaes_dec_ctx);
+    WBCRYPTO_wbaes_wbgcm_mask_setiv(wbgcm_dec, iv, sizeof(iv));
+    WBCRYPTO_wbaes_wbgcm_mask_aad(wbgcm_dec, aad, sizeof(aad));
+
+    for (i = 0; i < sizeof(testFilePath) / 8; i++) {
+        char *tmp_enc = "../../testData/test.enc.txt";
+        char *tmp_dec = "../../testData/test.dec.txt";
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_wbaes_wbgcmfile_mask_encrypt(wbgcm_enc, WBCRYPTO_TEST_FIN_PATH, WBCRYPTO_TEST_FENC_PATH);
+
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBSM4-Mask encrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+
+        program_start = clock();
+        for (j = 0; j < TEST_CYCLE_NUM; j++) {
+            WBCRYPTO_wbaes_wbgcmfile_mask_decrypt(wbgcm_dec, WBCRYPTO_TEST_FENC_PATH, WBCRYPTO_TEST_FDEC_PATH);
+        }
+        program_end = clock();
+        ts = program_end - program_start;
+        ts = ts / CLOCKS_PER_SEC;
+        printf("WBSM4-Mask decrypt %d KB txt spend: %lf s\n", power(10, i), ts / TEST_CYCLE_NUM);
+        remove(tmp_enc);
+        remove(tmp_dec);
+    }
+
+    WBCRYPTO_wbaes_wbgcm_mask_free(wbgcm_enc);
+    WBCRYPTO_wbaes_wbgcm_mask_free(wbgcm_dec);
+    return 1;
+}
+
+int main() {
+    test_aes_gcmfile();
+    test_sm4_gcmfile();
+    test_wbaes_gcmfile();
+    test_wbsm4_gcmfile();
+    test_wbaes_wbgcmfile_ee();
+    test_wbaes_wbgcmfile_mask();
+}
--- test/perform_wbaes_test.c
+++ test/perform_wbaes_test.c
@@ -2,120 +2,128 @@
 #include <wbcrypto/wbaes.h>
 #include <omp.h>
 
-static const unsigned char key[16]={0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
-                                    0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
+static const unsigned char key[16] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+                                      0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
 
-static const unsigned char msg[16]={0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
-                                    0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
+static const unsigned char msg[16] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+                                      0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10};
 
-#define TESTTIME 10
-
-void test_wbaes_standard_perf(){
+void test_wbaes_standard_perf() {
     int i;
     unsigned char cipher[16] = {0};
-    double start,end,total;
+    double start, end, total;
 
     WBCRYPTO_wbaes_context *wbaes_ctx_enc;
-    wbaes_ctx_enc=WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
+    wbaes_ctx_enc = WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
     WBCRYPTO_wbaes_gen_table(wbaes_ctx_enc, key, sizeof(key));
 
-    start=omp_get_wtime();
-    for (i = 0; i < TESTTIME*64*1024; i++) {
+    start = omp_get_wtime();
+    for (i = 0; i < TEST_CYCLE_NUM * 64 * 1024; i++) {
         WBCRYPTO_wbaes_encrypt(msg, cipher, wbaes_ctx_enc);
     }
-    end=omp_get_wtime();
+    end = omp_get_wtime();
     total = end - start;
-    printf("The standard of WBAES encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n", total / TESTTIME, 1/(total/TESTTIME));
+    printf("The standard of WBAES encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n",
+           total / TEST_CYCLE_NUM, 1 / (total / TEST_CYCLE_NUM));
 
     WBCRYPTO_wbaes_context_free(wbaes_ctx_enc);
 }
 
-void test_wbaes_cbc_perf(){
+void test_wbaes_cbc_perf() {
     int i;
-    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
     unsigned char msg1024[1024] = {0};
     unsigned char cipher1024[1024] = {0};
     size_t use_len;
-    for(i=0;i<1024;i++){
-        msg1024[i]=i & 0xff;
+    for (i = 0; i < 1024; i++) {
+        msg1024[i] = i & 0xff;
     }
-    double start,end,total;
+    double start, end, total;
 
     WBCRYPTO_wbaes_context *wbaes_ctx_enc;
-    wbaes_ctx_enc=WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
+    wbaes_ctx_enc = WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
     WBCRYPTO_wbaes_gen_table(wbaes_ctx_enc, key, sizeof(key));
 
-    start=omp_get_wtime();
-    for (i = 0; i < TESTTIME*1024; i++) {
-        WBCRYPTO_wbaes_cbc_encrypt(msg1024, sizeof(msg1024), cipher1024, sizeof(cipher1024), &use_len, wbaes_ctx_enc, iv);
+    start = omp_get_wtime();
+    for (i = 0; i < TEST_CYCLE_NUM * 1024; i++) {
+        WBCRYPTO_wbaes_cbc_encrypt(msg1024, sizeof(msg1024), cipher1024, sizeof(cipher1024), &use_len, wbaes_ctx_enc,
+                                   iv);
     }
-    end=omp_get_wtime();
+    end = omp_get_wtime();
     total = end - start;
-    printf("The cbc mode of WBAES encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n", total / TESTTIME, 1/(total/TESTTIME));
+    printf("The cbc mode of WBAES encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n",
+           total / TEST_CYCLE_NUM, 1 / (total / TEST_CYCLE_NUM));
     WBCRYPTO_wbaes_context_free(wbaes_ctx_enc);
 }
 
-void test_wbaes_gcm_perf(){
+void test_wbaes_gcm_perf() {
     int i;
-    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
     unsigned char msg1024[1024] = {0};
     unsigned char cipher1024[1024] = {0};
-    for(i=0;i<1024;i++){
-        msg1024[i]=i & 0xff;
+    for (i = 0; i < 1024; i++) {
+        msg1024[i] = i & 0xff;
     }
-    double start,end,total;
+    double start, end, total;
 
     WBCRYPTO_wbaes_context *wbaes_ctx_enc;
-    wbaes_ctx_enc=WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
+    wbaes_ctx_enc = WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
     WBCRYPTO_wbaes_gen_table(wbaes_ctx_enc, key, sizeof(key));
 
     WBCRYPTO_gcm_context *gcm_enc;
-    gcm_enc=WBCRYPTO_wbaes_gcm_init(wbaes_ctx_enc);
-    WBCRYPTO_gcm_setiv(gcm_enc,iv,sizeof(iv));
-    WBCRYPTO_gcm_aad(gcm_enc,aad,sizeof(aad));
-    start=omp_get_wtime();
-    for (i = 0; i < TESTTIME*1024; i++) {
-        WBCRYPTO_gcm_encrypt(gcm_enc,msg1024,sizeof(msg1024),cipher1024,sizeof(cipher1024));
+    gcm_enc = WBCRYPTO_wbaes_gcm_init(wbaes_ctx_enc);
+    WBCRYPTO_gcm_setiv(gcm_enc, iv, sizeof(iv));
+    WBCRYPTO_gcm_aad(gcm_enc, aad, sizeof(aad));
+    start = omp_get_wtime();
+    for (i = 0; i < TEST_CYCLE_NUM * 1024; i++) {
+        WBCRYPTO_gcm_encrypt(gcm_enc, msg1024, sizeof(msg1024), cipher1024, sizeof(cipher1024));
     }
-    end=omp_get_wtime();
+    end = omp_get_wtime();
     total = end - start;
-    printf("The gcm mode of WBAES encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n", total / TESTTIME, 1/(total/TESTTIME));
+    printf("The gcm mode of WBAES encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n",
+           total / TEST_CYCLE_NUM, 1 / (total / TEST_CYCLE_NUM));
     WBCRYPTO_wbaes_context_free(wbaes_ctx_enc);
     WBCRYPTO_gcm_free(gcm_enc);
 }
 
-int test_wbaes_gcm_parallel_perf(){
+int test_wbaes_gcm_parallel_perf() {
     int i;
-    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
-    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    unsigned char iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                            0x0f};
+    unsigned char aad[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+                             0x0f};
     unsigned char msg1024[1024] = {0};
     unsigned char cipher1024[1024] = {0};
-    for(i=0;i<1024;i++){
-        msg1024[i]=i & 0xff;
+    for (i = 0; i < 1024; i++) {
+        msg1024[i] = i & 0xff;
     }
-    double start,end,total;
+    double start, end, total;
 
     WBCRYPTO_wbaes_context *wbaes_ctx_enc;
-    wbaes_ctx_enc=WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
+    wbaes_ctx_enc = WBCRYPTO_wbaes_context_init(WBCRYPTO_ENCRYPT_MODE, 1);
     WBCRYPTO_wbaes_gen_table(wbaes_ctx_enc, key, sizeof(key));
 
     WBCRYPTO_gcm_context *gcm_enc;
-    gcm_enc=WBCRYPTO_wbaes_gcm_init(wbaes_ctx_enc);
-    WBCRYPTO_gcm_setiv(gcm_enc,iv,sizeof(iv));
-    WBCRYPTO_gcm_aad(gcm_enc,aad,sizeof(aad));
-    start=omp_get_wtime();
-    for (i = 0; i < TESTTIME*1024; i++) {
-        WBCRYPTO_gcm_parallel_encrypt(gcm_enc,msg1024,sizeof(msg1024),cipher1024,sizeof(cipher1024));
+    gcm_enc = WBCRYPTO_wbaes_gcm_init(wbaes_ctx_enc);
+    WBCRYPTO_gcm_setiv(gcm_enc, iv, sizeof(iv));
+    WBCRYPTO_gcm_aad(gcm_enc, aad, sizeof(aad));
+    start = omp_get_wtime();
+    for (i = 0; i < TEST_CYCLE_NUM * 1024; i++) {
+        WBCRYPTO_gcm_parallel_encrypt(gcm_enc, msg1024, sizeof(msg1024), cipher1024, sizeof(cipher1024));
     }
-    end=omp_get_wtime();
+    end = omp_get_wtime();
     total = end - start;
-    printf("The gcm mode of WBAES parallel encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n", total / TESTTIME, 1/(total/TESTTIME));
+    printf("The gcm mode of WBAES parallel encrypt 1MB spend:  %15.15f s, it means that the encryption speed is: %f MByte/s\n",
+           total / TEST_CYCLE_NUM, 1 / (total / TEST_CYCLE_NUM));
     WBCRYPTO_wbaes_context_free(wbaes_ctx_enc);
     WBCRYPTO_gcm_free(gcm_enc);
 }
 
-int main(){
+int main() {
     test_wbaes_standard_perf();
     test_wbaes_cbc_perf();
     test_wbaes_gcm_perf();
