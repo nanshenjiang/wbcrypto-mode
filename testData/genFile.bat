05ee26d90fc3ea0b0e4db7fec105fbc2ff12412e
nanshenjiang <979114715@qq.com>
2023-03-26 17:19:02 +0800

    feat: shell of generating file

---
+++ testData/genFile.bat
@@ -0,0 +1,6 @@
+fsutil file createnew test_1KB.txt 1024
+fsutil file createnew test_10KB.txt 10240
+fsutil file createnew test_100KB.txt 102400
+fsutil file createnew test_1MB.txt 1048576
+fsutil file createnew test_10MB.txt 10485760
+fsutil file createnew test_100MB.txt 104857600
---
+++ testData/genFile.sh
@@ -0,0 +1,6 @@
+dd if=/dev/zero of=test_1KB.txt bs=1K count=1
+dd if=/dev/zero of=test_10KB.txt bs=10K count=1
+dd if=/dev/zero of=test_100KB.txt bs=100K count=1
+dd if=/dev/zero of=test_1MB.txt bs=1M count=1
+dd if=/dev/zero of=test_10MB.txt bs=10M count=1
+dd if=/dev/zero of=test_100MB.txt bs=100M count=1
